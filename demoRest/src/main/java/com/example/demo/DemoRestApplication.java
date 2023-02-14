44e4573e516b6dc8b26667eac1bb388d0edd8af3
nimith <nimithbbz10@gmail.com>
2023-02-14 21:51:50 +0530

    Add files via upload

---
+++ demoRest/src/main/java/com/example/demo/DemoRestApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoRestApplication {
+
+	@RequestMapping(value="/")
+	public String index() {
+		return "<h1>Hello World</h1>";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestApplication.class, args);
+	}
+}
